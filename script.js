b10165039980ee1dde1b2d05f86d25c4eb74e168
emanoelceccon <emanoel.ceccon@escola.pr.gov.br>
2025-09-15 11:02:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const botaoMostrarPalavras = document.querySelector("#botao-palavrachave")
-botaoMostrarPalavras.addEventListener("click" , MostraPalavrasChave);
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click" , MostraPalavrasChave);
 function MostraPalavrasChave(){
 const texto = document.querySelector("#entrada-de-texto").value;
 const campoResultado = document.querySelector("#resultado-palavrachave");
 campoResultado.textContent = texto;
-const palavras = texto.split(" ");
+const palavrasChave = processaTexto(texto);
 campoResultado.textContent = palavras.join(" , ");
+}
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
